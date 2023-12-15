8ed8eed2d3b6cfd4ca0e489f82a840c39fb32a82
Morgan Jones <me@morganlabs.dev>
2023-12-15 15:02:03 +0000

    fix: Add blank loading time whilst styles load

---
+++ src/app.js
@@ -0,0 +1,6 @@
+document.body.style.visibility = "hidden";
+
+// Wait for styles to load before showing content
+window.addEventListener("load", function() {
+    document.body.style.visibility = "visible";
+});
