8baa3aa7e7379d00a3d0b0c07fe493f020f14356
Morgan Jones <me@morganlabs.dev>
2023-12-15 14:48:06 +0000

    feat: Small card component

--- src/components/cards.js
+++ src/components/cards.js
@@ -8,19 +8,19 @@ class Hero extends HTMLElement {
         wholePath.splice(wholePath.indexOf("src") + 1);
         const pathFromSrc = wholePath.join("/");
 
-        const type = this.parseType(this.getAttribute("type"));
-        const location = this.parseLocation(this.getAttribute("location"));
+        const type = parseType(this.getAttribute("type"));
+        const location = parseLocation(this.getAttribute("location"));
         const title = this.getAttribute("title");
-        const showInfoPath = this.getShowInfoPage(this.getAttribute("show-id"), pathFromSrc);
-        const venueCymruLink = this.getVcLink(this.getAttribute("vc-slug"));
+        const showInfoPath = getShowInfoPage(this.getAttribute("show-id"), pathFromSrc);
+        const venueCymruLink = getVcLink(this.getAttribute("vc-slug"));
         const backgroundImage = `${pathFromSrc}/` + this.getAttribute("background-image");
 
         this.innerHTML = `
             <article class="card hero">
-                <div class="small-info">
-                    <span class="type">${type}</span>
-                    <span class="seperator"></span>
-                    <span class="location">${location}</span>
+                    <div class="small-info">
+                        <span class="type">${type}</span>
+                        <span class="seperator"></span>
+                        <span class="location">${location}</span>
                 </div>
                 <h1>${title}</h1>
                 <div class="action-row">
@@ -28,31 +28,70 @@ class Hero extends HTMLElement {
                     <a href="${showInfoPath}" class="cmp-btn secondary">Read More</a>
                 </div>
                 <div class="gradient"></div>
-                <img class="background" src="${backgroundImage}" alt="Hero image for ${title}">
+                <img class="image" src="${backgroundImage}" alt="Hero image for ${title}">
+            </article>
+            `
+    }
+
+}
+
+class Small extends HTMLElement {
+    static get observedAttributes() {
+        return ["type", "location", "title", "show-id", "vc-slug", "image"];
+    }
+
+    connectedCallback() {
+        let wholePath = window.location.pathname.split("/");
+        wholePath.splice(wholePath.indexOf("src") + 1);
+        const pathFromSrc = wholePath.join("/");
+
+        const type = parseType(this.getAttribute("type"));
+        const location = parseLocation(this.getAttribute("location"));
+        const title = this.getAttribute("title");
+        const showInfoPath = getShowInfoPage(this.getAttribute("show-id"), pathFromSrc);
+        const venueCymruLink = getVcLink(this.getAttribute("vc-slug"));
+        const image = `${pathFromSrc}/` + this.getAttribute("image");
+
+        this.innerHTML = `
+            <article class="card small">
+                <img class="image" src="${image}" alt="Hero image for ${title}">
+               <div class="small-info">
+                    <span class="type">${type}</span>
+                    <span class="seperator"></span>
+                    <span class="location">${location}</span>
+                </div>
+                <h2>${title}</h2>
+                <div class="action-row">
+                    <a href="${venueCymruLink}" class="cmp-btn primary">Buy tickets</a>
+                    <a href="${showInfoPath}" class="cmp-btn secondary">Read More</a>
+                </div>
             </article>
         `
     }
+}
 
-    parseType(type) {
-        switch (type) {
-            case "MUS": return "Musical";
-        }
-    }
-
-    parseLocation(location) {
-        switch (location) {
-            case "VenCy": return "Venue Cymru";
-        }
-    }
-
-    getVcLink(slug) {
-        return `https://www.venuecymru.co.uk/${slug}`;
-    }
-
-    getShowInfoPage(id, pathFromSrc) {
-        return `${pathFromSrc}/whats-on/${id}`;
+function parseType(type) {
+    switch (type) {
+        case "MUS": return "Musical";
+        case "FIL": return "Film";
     }
 }
 
+function parseLocation(location) {
+    switch (location) {
+        case "VenCy": return "Venue Cymru";
+        case "TheCo": return "Theatr Colwyn"
+    }
+}
+
+function getVcLink(slug) {
+    return `https://www.venuecymru.co.uk/${slug}`;
+}
+
+function getShowInfoPage(id, pathFromSrc) {
+    return `${pathFromSrc}/whats-on/${id}`;
+}
+
 customElements.define("card-hero", Hero);
+customElements.define("card-small", Small);
 
